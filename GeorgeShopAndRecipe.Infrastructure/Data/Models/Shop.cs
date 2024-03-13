cea13c2b5c60f89724ec212e5f5bd1898da05f8d
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2024-03-13 22:11:05 +0300

    Adding classes Shop and Supplier to database

---
+++ GeorgeShopAndRecipe.Infrastructure/Data/Models/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static GeorgeShopAndRecipe.Infrastructure.Data.Constants.DataConstants;
+namespace GeorgeShopAndRecipe.Infrastructure.Data.Models
+{
+    public class Shop
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ShopNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression("^\\+359[7-9][0-9]{8}$")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+
+        [Required]
+        public ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
+    }
+}
---
+++ GeorgeShopAndRecipe.Infrastructure/Data/Models/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static GeorgeShopAndRecipe.Infrastructure.Data.Constants.DataConstants;
+namespace GeorgeShopAndRecipe.Infrastructure.Data.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(SupplierFirstNameMaxLength)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(SupplierLastNameMaxLength)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(SupplierAgeMinValue,SupplierAgeMaxValue)]
+        public int Age { get; set; }
+
+        [Range(SupplierRatingMinValue, SupplierRatingMaxValue)]
+        public double Rating { get; set; }
+    }
+}
