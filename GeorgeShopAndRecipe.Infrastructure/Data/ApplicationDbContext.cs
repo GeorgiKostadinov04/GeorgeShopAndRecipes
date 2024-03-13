4da24836df7444a8283bec0c29eff62bac8adb42
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2024-03-13 22:42:49 +0300

    Added DbContext

--- GeorgeShopAndRecipe.Infrastructure/Data/ApplicationDbContext.cs
+++ GeorgeShopAndRecipe.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GeorgeShopAndRecipe.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GeorgeShopAndRecipe.Infrastructure.Data
@@ -8,6 +9,54 @@ namespace GeorgeShopAndRecipe.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public DbSet<Shop> Shops { get; set; }
+
+        public DbSet<Ingredient> Ingredients { get; set;}
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Category>()
+                .HasData(new Category()
+                {
+                    Id = 1,
+                    Name = "Gluten free"
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Vegetarian"
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "Vegan"
+                },
+                new Category()
+                {
+                    Id = 4,
+                    Name = "Dairy free"
+                },
+                new Category()
+                {
+                    Id = 5,
+                    Name = "High protein"
+                },
+                new Category()
+                {
+                    Id = 6,
+                    Name = "None"
+                });
+
+
+            base.OnModelCreating(builder);
         }
     }
 }
