90261d994f431986ad41ad7c24c4d953ab3c750c
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2024-03-19 21:44:08 +0300

    update db context

--- GeorgeShopAndRecipe.Infrastructure/Data/ApplicationDbContext.cs
+++ GeorgeShopAndRecipe.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using GeorgeShopAndRecipe.Infrastructure.Data.Models;
+using GeorgeShopAndRecipe.Infrastructure.Data.SeedDb;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,14 +18,52 @@ namespace GeorgeShopAndRecipe.Infrastructure.Data
 
         public DbSet<Shop> Shops { get; set; }
 
-        public DbSet<Ingredient> Ingredients { get; set;}
+        public DbSet<Ingredient> Ingredients { get; set; }
 
         public DbSet<Supplier> Suppliers { get; set; }
 
         public DbSet<RecipeDeveloper> RecipeDevelopers { get; set; }
 
+        public DbSet<IngredientsShops> IngredienstShops { get; set; }
+
+        public DbSet<IngredientsRecipes> IngredientsRecipes { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+
+            builder.Entity<IngredientsShops>()
+                .HasKey(x => new { x.ShopId, x.IngredientId });
+
+            builder.Entity<IngredientsRecipes>()
+                .HasKey(x => new { x.RecipeId, x.IngredientId });
+
+            builder.Entity<IngredientsRecipes>()
+                .HasOne(x => x.Ingredient)
+                .WithMany(y => y.IngredientsRecipes)
+                .HasForeignKey(x => x.IngredientId);
+
+            builder.Entity<IngredientsRecipes>()
+                .HasOne(x=> x.Recipe)
+                .WithMany(y => y.IngredientsRecipes)
+                .HasForeignKey(x => x.RecipeId);
+
+            builder.Entity<IngredientsShops>()
+                .HasOne(x => x.Ingredient)
+                .WithMany(y => y.IngredientsShops)
+                .HasForeignKey(x => x.IngredientId);
+
+            builder.Entity<IngredientsShops>()
+                .HasOne(x => x.Shop)
+                .WithMany(y => y.IngredientsShops)
+                .HasForeignKey(x => x.ShopId);
+
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new RecipeConfiguration());
+            builder.ApplyConfiguration(new RecipeDeveloperConfiguration());
+            builder.ApplyConfiguration(new ShopConfiguration());
+            builder.ApplyConfiguration(new SupplierConfiguration());
+            builder.ApplyConfiguration(new IngredientConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
 
             base.OnModelCreating(builder);
         }
