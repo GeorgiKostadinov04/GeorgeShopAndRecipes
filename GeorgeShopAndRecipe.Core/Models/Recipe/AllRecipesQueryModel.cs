a6f7c3e9fc7416ce3ce8cf83b33d9e78ee6a1922
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2024-03-31 20:22:58 +0300

    All recipe quary model implemented

--- GeorgeShopAndRecipe.Core/Models/Recipe/AllRecipesQueryModel.cs
+++ GeorgeShopAndRecipe.Core/Models/Recipe/AllRecipesQueryModel.cs
@@ -1,7 +1,24 @@
-﻿namespace GeorgeShopAndRecipe.Core.Models.Recipe
+﻿using GeorgeShopAndRecipe.Core.Contracts.Recipe;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace GeorgeShopAndRecipe.Core.Models.Recipe
 {
     public class AllRecipesQueryModel
     {
+        public const int RecipePerPage = 3;
 
+        public string Category { get; set; } = null!;
+
+        [Display(Name = "Search by text")]
+        public string SearchItem { get; set; } = null!;
+
+        public int CurrentPage { get; set; } = 1;
+
+        public int TotalRecipesCount { get; set; }
+
+        public IEnumerable<string> Categories { get; set; } = null!;
+
+        public IEnumerable<RecipeServiceModel> Recipes { get; set; } = new List<RecipeServiceModel>();
     }
 }
