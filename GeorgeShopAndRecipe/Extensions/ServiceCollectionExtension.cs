75a79aff67818d79c97fd9a11c55dcf984230dc7
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2024-04-06 17:46:21 +0300

    fixed program issue

--- GeorgeShopAndRecipe/Extensions/ServiceCollectionExtension.cs
+++ GeorgeShopAndRecipe/Extensions/ServiceCollectionExtension.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             services.AddScoped<IRecipeService, RecipeService>();
             services.AddScoped<IRecipeDeveloperService, RecipeDeveloperService>();
+            services.AddScoped<IStatisticService, StatisticService>();
             return services;
         }
 
