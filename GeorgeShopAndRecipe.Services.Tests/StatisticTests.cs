a8886d659ecfc6fa84b1e9e8106aa95b31055290
Георги Цветанов Костадинов <Georgi.Kostadinov777@gmail.com>
2024-04-17 21:01:04 +0300

    Unit tests added

---
+++ GeorgeShopAndRecipe.Services.Tests/StatisticTests.cs
@@ -0,0 +1,66 @@
+﻿using GeorgeShopAndRecipe.Core.Contracts.Recipe;
+using GeorgeShopAndRecipe.Infrastructure.Common;
+using GeorgeShopAndRecipe.Infrastructure.Data.Models;
+using GeorgeShopAndRecipe.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeorgeShopAndRecipe.Core.Contracts;
+using GeorgeShopAndRecipe.Core.Services;
+using GeorgeShopAndRecipe.Core.Models.Statistics;
+
+namespace GeorgeShopAndRecipe.Services.Tests
+{
+    [TestFixture]
+    public class StatisticTests
+    {
+        private IRepository repository;
+        private IStatisticService service;
+        private ApplicationDbContext context;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("RecipeDB")
+                .Options;
+
+            context = new ApplicationDbContext(contextOptions);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        [Test]
+        public async Task StatisticsTotalAsyncMethodShouldReturnTheRightType()
+        {
+            repository = new Repository(context);
+            service = new StatisticService(repository);
+
+            StatisticsServiceModel goal = new StatisticsServiceModel();
+
+            var result = await service.TotalAsync();
+
+            Assert.That(result.GetType(), Is.EqualTo(goal.GetType()));
+        }
+
+        [Test]
+        public async Task StatisticsTotalAsyncMethodShouldWorkProperly()
+        {
+            repository = new Repository(context);
+            service = new StatisticService(repository);
+
+            var result = await service.TotalAsync();
+
+            var count = result.TotalRecipes;
+            //Seeded in the database
+            Assert.That(count, Is.EqualTo(2));
+        }
+    }
+}
---
+++ GeorgeShopAndRecipe.Services.Tests/UserTests.cs
@@ -0,0 +1,64 @@
+﻿using GeorgeShopAndRecipe.Core.Contracts;
+using GeorgeShopAndRecipe.Core.Services;
+using GeorgeShopAndRecipe.Infrastructure.Common;
+using GeorgeShopAndRecipe.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeorgeShopAndRecipe.Services.Tests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        private IRepository repository;
+        private IUserService service;
+        private ApplicationDbContext context;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("RecipeDB")
+                .Options;
+
+            context = new ApplicationDbContext(contextOptions);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        [Test]
+
+        public async Task UserAllAsyncMethodShouldWorkProperly()
+        {
+            repository = new Repository(context);
+            service = new UserService(repository);
+
+            var result = await service.AllAsync();
+
+            var count = result.Count();
+
+            Assert.That(count, Is.Not.NaN);
+        }
+
+        [Test]
+
+        public async Task UserFullNameAsyncShouldWorkProperly()
+        {
+            repository = new Repository(context);
+            service = new UserService(repository);
+            
+            string userId = "299e94d7-1a68-4738-9d25-e062394ae0c5";
+
+            string result = await service.UserFullNameAsync(userId);
+            //Seeded in the database
+            Assert.That(result, Is.EqualTo("Great Admin"));
+        }
+    }
+}
